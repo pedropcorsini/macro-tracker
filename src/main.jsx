9ed4454442b1e992e54151eaf1c2a6f14292a085
Pedro Passos Corsini <pedro.pcorsini@gmail.com>
2026-06-16 23:43:37 -0300

    feat(app): integrar shell autenticado

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import "./i18n"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { ThemeProvider } from "./context/ThemeContext"
+import "./index.css"
+import App from "./App.jsx"
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+)
